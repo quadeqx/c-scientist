e09eefb84a39743b6ec60ddda67604f61c58983b
brantyyn <brantonmaungu@gmail.com>
2025-06-18 21:54:34 +0300

    Set watchlist data

--- charts/__init__.py
+++ charts/__init__.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 17 09:23:59 2025
+from datetime import datetime
 
-@author: quantum
-"""
+def preprocess_binance_data(binance_data):
+    for candle in binance_data:
+        processed = {
+            "Open": float(candle["open"]),
+            "High": float(candle["high"]),
+            "Low": float(candle["low"]),
+            "Close": float(candle["close"])
+        }
+    return processed
--- data/watchlists/list1.py
+++ data/watchlists/list1.py
@@ -1,75 +1,87 @@
-import pyqtgraph as pg
 from PyQt5.QtWidgets import QWidget, QGridLayout
 from pyqtgraph import TableWidget
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore
+from data.coins.coin_data import coinprice, Data
 
 class WatchlistManager(QWidget):
     def __init__(self):
         super().__init__()
 
         self.data_manager = Data()
+        self.coinprice = coinprice()
 
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
         payments = TableWidget(10, 6)
-        payments.setData(self.data_manager.get_watchlist_by_key('payments'))
-        payments.setHorizontalHeaderLabels(['Payments'])
-        payments.resizeColumnsToContents()
-        self.layout.addWidget(payments, 0, 0)
+        while True:
+            data0 = self.coinprice.getprices('payments')
+            processed = self.coinprice.prepare_table_data(data0)
+            payments.setData(processed)
+            payments.setHorizontalHeaderLabels(['Payments'])
+            payments.resizeColumnsToContents()
+            self.layout.addWidget(payments, 0, 0)
 
-
+"""
         ai = TableWidget(10, 1)
-        ai.setData(self.data_manager.get_watchlist_by_key('ai'))
+        data1 = self.coinprice.getprices('ai')
+        ai.setData(self.coinprice.prepare_table_data(data1))
         ai.setHorizontalHeaderLabels(['AI'])
         ai.resizeColumnsToContents()
         self.layout.addWidget(ai, 1, 0)
 
 
         meme = TableWidget(10, 1)
-        meme.setData(self.data_manager.get_watchlist_by_key('meme'))
+        meme_data = self.coinprice.getprices('meme')
+        meme.setData(self.coinprice.prepare_table_data(meme_data))
         meme.setHorizontalHeaderLabels(['Meme'])
         meme.resizeColumnsToContents()
         self.layout.addWidget(meme, 0, 1)
 
 
         l3 = TableWidget(10, 1)
-        l3.setData(self.data_manager.get_watchlist_by_key('L_3'))
+        l3_data = self.coinprice.getprices('L_3')
+        l3.setData(self.coinprice.prepare_table_data(l3_data))
         l3.setHorizontalHeaderLabels(['Layer 3'])
         l3.resizeColumnsToContents()
         self.layout.addWidget(l3, 1, 1)
 
 
         l1 = TableWidget(10, 1)
-        l1.setData(self.data_manager.get_watchlist_by_key('L_1'))
+        l1_data = self.coinprice.getprices('L_1')
+        l1.setData(self.coinprice.prepare_table_data(l1_data))
         l1.setHorizontalHeaderLabels(['Layer 1'])
         l1.resizeColumnsToContents()
         self.layout.addWidget(l1, 2, 1)
 
 
         dexchange = TableWidget(10, 1)
-        dexchange.setData(self.data_manager.get_watchlist_by_key('dexchange'))
+        dexchange_data = self.coinprice.getprices('dexchange')
+        dexchange.setData(self.coinprice.prepare_table_data(dexchange_data))
         dexchange.setHorizontalHeaderLabels(['Dex Exchange'])
         dexchange.resizeColumnsToContents()
         self.layout.addWidget(dexchange, 1, 2)
 
 
         liquid_staking = TableWidget()
-        liquid_staking.setData(self.data_manager.get_watchlist_by_key('liquid_staking'))
+        liquid_staking_data = self.coinprice.getprices('liquid_staking')
+        liquid_staking.setData(self.coinprice.prepare_table_data(liquid_staking_data))
         liquid_staking.setHorizontalHeaderLabels(['Liquid Staking'])
         liquid_staking.resizeColumnsToContents()
         self.layout.addWidget(liquid_staking, 0, 2)
 
 
         l0 = TableWidget()
-        l0.setData(self.data_manager.get_watchlist_by_key('L_0'))
+        l0_data = self.coinprice.getprices('L_0')
+        l0.setData(self.coinprice.prepare_table_data(l0_data))
         l0.setHorizontalHeaderLabels(['Layer 0'])
         l0.resizeColumnsToContents()
         self.layout.addWidget(l0, 2, 0)
 
 
         l2 = TableWidget()
-        l2.setData(self.data_manager.get_watchlist_by_key('L_2'))
+        l2_data = self.coinprice.getprices('L_2')
+        l2.setData(self.coinprice.prepare_table_data(l2_data))
         l2.setHorizontalHeaderLabels(['Layer 2'])
         delegate = CenteredItemDelegate(l2)
         l2.setItemDelegate(delegate)
@@ -82,7 +94,7 @@ class WatchlistManager(QWidget):
         for idx, item in enumerate(lists):
             item.setItemDelegate(CenteredItemDelegate(item))
 
-
+    """
 
 
 
@@ -95,22 +107,4 @@ class CenteredItemDelegate(QtWidgets.QStyledItemDelegate):
         option.displayAlignment = QtCore.Qt.AlignCenter
 
 
-class Data:
-    def __init__(self):
-        self.watchlists = {
-            "payments": ["BTC", "XRP", "BCH", "ARK", "PEPE", "TRX", "DOGE", "VRA", "DOGE"],
-            "ai": ["LTC", "AI", "TOKEN", "TAO", "NEAR", "WLD", "AIXBT", "FET", "RENDER", "ALU", "FET"],
-            "meme": ["FART", "TRUMP", "SHIB", "BONK", "NOT", "MOODENCY", "BOME", "PEOPLE", "JELLYJELLY", ],
-            "analytics": ["ARKM", "CGPT"],
-            "dexchange": ["BNB", "INJ", "FTT", "ARKM", "TKO"],
-            "liquid_staking": ["WBETH", "LDO", "ANKR", "RPL", "LISTA", "OGN", "QI", "HAEDAL", "FIS", "CHESS"],
-            "L_0": ["ATOM", "DOT", "DATA", "ZRO", "AVAX"],
-            "L_1": ["HYPE", "ADA", "FIL", "TON", "APT", "TAO", "TIA", "KAVA", "PLUME", "XCN", "VANRY"],
-            "L_2": ["OP", "MNT", "CYBER", "ARB", "STX", "IMX","DYDX", "SNX", "ZK", "ZRX","STRK", "*KERNEL",],
-            "L_3": ["XAI","DEGEN", "ORBS", "GHST"]
 
-
-        }
-
-    def get_watchlist_by_key(self, key):
-        return [[item] for item in self.watchlists.get(key, [])]
